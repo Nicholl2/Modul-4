c3e93c615786e106aeff3045169ac7647732ac79
Nicholas Anindya Dinata <152374967+Nicholl2@users.noreply.github.com>
2024-03-29 00:51:04 +0700

    Delete Pinjam_Buku.py

--- Pinjam_Buku.py
+++
@@ -1,8 +0,0 @@
-class Fitur1:
-    def Pinjam_buku(self, Buku, Peminjam):
-        if Buku.yang_tersedia > 0:
-            Buku.yang_tersedia -= 1
-            return f"{Buku.Judul} telah dipinjam oleh {Peminjam}."
-        else:
-            return f"Maaf, {Buku.Judul} sedang tidak tersedia"
-
